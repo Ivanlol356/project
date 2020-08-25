46e949caa2d19fb07f5f9d6e8f5f6b7ae5498958
Ivanlol356 <sivant333@gmail.com>
2020-08-26 00:59:40 +0400

    desktop

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+$(".slider-wrap").slick({
+    prevArrow: '<button id="prev" type="button" class="btn-prev"></button>',
+    nextArrow: '<button id="next" type="button" class="btn-next"></button>'
+});
+$(".news-box").slick({
+    prevArrow: '<button id="prev" type="button" class="btn-prev"></button>',
+    nextArrow: '<button id="next" type="button" class="btn-next"></button>'
+});
+});
